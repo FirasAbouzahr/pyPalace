fixed units in EPR analysis calculations

pypalace/analysis.py
@@ -18,7 +18,7 @@ class EPR:
         return -alpha_q
     
     ## from https://arxiv.org/pdf/2010.00620 (equation 11)
-    def calculate_dispersive_shift(p_q,p_r,f_q,f_r,LJ):
+    def calculate_dispersive_shift(p_q,p_r,f_q,f_r,LJ): # unitless, unitless, GHz, GHz, Henries
         
 
         EJ = phi0**2/((2*pi)**2*LJ)
@@ -37,8 +37,8 @@ class EPR:
     
     ## from https://arxiv.org/pdf/2312.13483 (equation 9)
     def calculate_coupling_strength(f_q,f_r,alpha_q,chi):
-        delta = (f_r - f_q)*1000
-        sigma = (f_q + f_r)*1000
+        delta = (f_r - f_q) * 10**9
+        sigma = (f_q + f_r) * 10**9
         denom = alpha_q/(delta *(delta - alpha_q)) + alpha_q/(sigma*(sigma + alpha_q))
         g = np.sqrt(chi/denom/2)
         return g