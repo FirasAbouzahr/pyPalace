ad343215da8cc43184038531f5327fcd8f759e3e
Firas Abouzahr <firasabouzahr@gmail.com>
2026-04-30 12:59:41 -0500

    updates to simulation & analysis codes

--- pypalace/analysis.py
+++ pypalace/analysis.py
@@ -7,36 +7,27 @@ phi0 = 2.0678338484619295e-15
 class EPR:
 
     ## from https://arxiv.org/pdf/2010.00620 (equation 9)
-    def calculate_anharmonicity(p_q,f_q,Ej = None,Lj = None): # f_q in GHz (default of Palace), Ej in Joules, Lj in Henries
+    def calculate_anharmonicity(p_q,f_q,LJ): # f_q in GHz (default of Palace), Ej in Joules, Lj in Henries
 
-        if Ej == None and Lj == None:
-            raise ValueError("Please enter a value for either Ej or Lj")
-        elif Ej != None and Lj != None:
-            print("Both Ej and Lj defined, defaulting to calculation using Ej")
-        elif Ej == None and Lj != None:
-            Ej = phi0**2/((2*pi)**2*Lj)
+        EJ = phi0**2/((2*pi)**2*LJ)
 
         w_q = 2 * np.pi * f_q * 10**9
-        alpha_q = p_q**2 * (hbar * w_q**2)/(8 * Ej) # calculate alpha
-        alpha_q = (alpha_q / (2*pi)) * 10**-6 # convert to MHz
+        alpha_q = p_q**2 * (hbar * w_q**2)/(8 * EJ) # calculate alpha
+        alpha_q = (alpha_q / (2*pi))
         
         return -alpha_q
     
     ## from https://arxiv.org/pdf/2010.00620 (equation 11)
-    def calculate_dispersive_shift(p_q,p_r,f_q,f_r,Ej = None,Lj = None):
+    def calculate_dispersive_shift(p_q,p_r,f_q,f_r,LJ):
         
-        if Ej == None and Lj == None:
-            raise ValueError("Please enter a value for either Ej or Lj")
-        elif Ej != None and Lj != None:
-            print("Both Ej and Lj defined, defaulting to calculation using Ej")
-        elif Ej == None and Lj != None:
-            Ej = phi0**2/((2*pi)**2*Lj)
+
+        EJ = phi0**2/((2*pi)**2*LJ)
 
         w_q = 2 * pi * f_q * 10**9
         w_r = 2 * pi * f_r * 10**9
 
-        chi = p_q * p_r * (hbar * w_q * w_r) / (4 * Ej) # calculate chi
-        chi = (chi / (2*pi)) * 10**-6 # convert to MHz
+        chi = p_q * p_r * (hbar * w_q * w_r) / (4 * EJ) # calculate chi
+        chi = (chi / (2*pi))
  
         return -chi
     
@@ -50,98 +41,6 @@ class EPR:
         sigma = (f_q + f_r)*1000
         denom = alpha_q/(delta *(delta - alpha_q)) + alpha_q/(sigma*(sigma + alpha_q))
         g = np.sqrt(chi/denom/2)
-        return g
-
-    def calculate_lamb_shift(alpha_q,chi):
-        return alpha_q - chi/2
-
-    def get_anharmonicity(self,qubit_mode,JJ_LumpedPort_index):
-        
-        with open(self.path_to_json, "r") as f:
-            this_config = json.load(f)
-        
-        output_folder = this_config["Problem"]["Output"]
-
-        # extract the JJ's indunctance (Lj) from config file based on LumpedPort index given
-        for lp in this_config["Boundaries"]["LumpedPort"]:
-            if lp["Index"] == JJ_LumpedPort_index:
-                Lj = lp["L"]
-
-        # convert to Ej
-        Ej = phi0**2/((2*np.pi)**2*Lj)
-
-        eigenvals = pd.read_csv(output_folder + "/eig.csv",usecols = [0,1])
-        eigenvals.columns = ["m","f"]
-        EPR = pd.read_csv(output_folder + "/port-EPR.csv")
-        EPR.columns = ["m","p"]
-
-        f_q = eigenvals[eigenvals.m == qubit_mode].f.iloc[0]
-        p_q = EPR[EPR.m == qubit_mode].p.iloc[0]
-
-        alpha_q = Simulation.calculate_anharmonicity(p_q,f_q,Ej)
-
-        return alpha_q
-
-    def get_dispersive_shift(self,qubit_mode,resonator_mode,JJ_LumpedPort_index):
-        
-        with open(self.path_to_json, "r") as f:
-            this_config = json.load(f)
-        
-        output_folder = this_config["Problem"]["Output"]
-
-        # extract the JJ's indunctance (Lj) from config file based on LumpedPort index given
-        for lp in this_config["Boundaries"]["LumpedPort"]:
-            if lp["Index"] == JJ_LumpedPort_index:
-                Lj = lp["L"]
-
-        # convert to Ej
-        Ej = phi0**2/((2*np.pi)**2*Lj)
-
-        eigenvals = pd.read_csv(output_folder + "/eig.csv",usecols = [0,1])
-        eigenvals.columns = ["m","f"]
-        EPR = pd.read_csv(output_folder + "/port-EPR.csv")
-        EPR.columns = ["m","p"]
-
-        f_q = eigenvals[eigenvals.m == qubit_mode].f.iloc[0]
-        f_r = eigenvals[eigenvals.m == resonator_mode].f.iloc[0]
-        p_q = EPR[EPR.m == qubit_mode].p.iloc[0]
-        p_r = EPR[EPR.m == resonator_mode].p.iloc[0]
-
-        chi = Simulation.calculate_dispersive_shift(p_q,p_r,f_q,f_r,Ej)
-
-        return chi
-
-    def get_lamb_shift(self,alpha_q,chi):
-        return Simulation.calculate_lamb_shift(alpha_q,chi)
-
-    def get_coupling_strength(self,qubit_mode,resonator_mode,JJ_LumpedPort_index):
-        
-        with open(self.path_to_json, "r") as f:
-            this_config = json.load(f)
-        
-        output_folder = this_config["Problem"]["Output"]
-
-        # extract the JJ's indunctance (Lj) from config file based on LumpedPort index given
-        for lp in this_config["Boundaries"]["LumpedPort"]:
-            if lp["Index"] == JJ_LumpedPort_index:
-                Lj = lp["L"]
-
-        # convert to Ej
-        Ej = phi0**2/((2*np.pi)**2*Lj)
-
-        eigenvals = pd.read_csv(output_folder + "/eig.csv",usecols = [0,1])
-        eigenvals.columns = ["m","f"]
-        EPR = pd.read_csv(output_folder + "/port-EPR.csv")
-        EPR.columns = ["m","p"]
-
-        f_q = eigenvals[eigenvals.m == qubit_mode].f.iloc[0]
-        f_r = eigenvals[eigenvals.m == resonator_mode].f.iloc[0]
-        p_q = EPR[EPR.m == qubit_mode].p.iloc[0]
-        p_r = EPR[EPR.m == resonator_mode].p.iloc[0]
-
-        alpha_q = Simulation.calculate_anharmonicity(p_q,f_q,Ej)
-        chi = Simulation.calculate_dispersive_shift(p_q,p_r,f_q,f_r,Ej)
-        g = Simulation.calculate_coupling_strength(f_q,f_r,alpha_q,chi)
         return g
         
 class LOM:
@@ -159,7 +58,8 @@ class LOM:
         alpha = qubit.anharmonicity() # in MHz
         
         return {"frequency_GHz":f_q,"anharmonicity_MHz":alpha}
-
-#class flux:
-#    
-#    def
+        
+        
+#class DrivenAnalysis:
+#
+#    def get_scatter_column
--- pypalace/simulation.py
+++ pypalace/simulation.py
@@ -7,12 +7,33 @@ from .tools import Tools
 
 class Simulation:
 
+
+
     def __init__(self,config:Config,path_to_palace:str):
+    
+        ''' 
+        
+        create Simulation object:
+        
+        config: pyPalace Config object
+        path_to_palace: path/to/your/palace/install
+    
+        '''
+    
+    
         self.path_to_palace = path_to_palace
         self.config = config
         self.path_to_json = self.config.config_name
         
     def HPC_options(partition,time,nodes,ntasks_per_node,mem,job_name,custom = None):
+    
+    
+        ''' 
+        
+        define slurm directives for running pyPalace simulations on HPC, input parameter into Simulation.run() 
+    
+        '''
+        
         
         partition = "partition={}".format(partition)
         time = "time={}".format(time)
@@ -30,7 +51,7 @@ class Simulation:
         return slurm_list
     
         
-    def run(self,n,HPC_options=None,custom_script_name = None):
+    def run(self,n,HPC_options=None,custom_script_name=None):
     
         if self.config.saved == False:
             self.config.save_config()
@@ -65,8 +86,12 @@ class Simulation:
                 print(command.stdout.strip())
                 print(command.stderr.strip())
                 
+                
+    def get_capacitance_matrix(self):
         
-        if self.config.sim["Problem"]["Type"] == "Electrostatic":
+        if self.config.sim["Problem"]["Type"] != "Electrostatic":
+            raise ValueError("Simulation type is not electrostatic, no capacitance matrix to extract")
+        else:
             cap_matrix_results = self.config.sim["Problem"]["Output"]+"/terminal-C.csv"
             cap_matrix = pd.read_csv(cap_matrix_results)
             cap_matrix = cap_matrix.drop(columns=['        i'])
@@ -81,12 +106,60 @@ class Simulation:
                 ID = terminal["Attributes"][0]
                 names.append(mesh_attributes[mesh_attributes.ID==str(ID)].Name.iloc[0])
             
-            
             cap_matrix.index = names
             cap_matrix.columns = names
             
             return cap_matrix
-                
-                
+    
+    def get_eigenFrequencies(self):
+    
+        if self.config.sim["Problem"]["Type"] != "Eigenmode":
+            raise ValueError("Simulation type is not eigenmode, no eigenfrequencies to extract")
             
+        else:
+        
+            freq_results = self.config.sim["Problem"]["Output"]+"/eig.csv"
+            freqs = pd.read_csv(freq_results,usecols = [0,1,2,3])
+            freqs.columns = ["mode","frequency_GHz","frequency_Im","Q"]
             
+            return freqs
+                
+    def get_portEPR(self,port_index:int):
+    
+        if self.config.sim["Problem"]["Type"] != "Eigenmode":
+            raise ValueError("Simulation type is not eigenmode, no port EPR to extract")
+            
+        try:
+            EPR_results = self.config.sim["Problem"]["Output"]+"/port-EPR.csv"
+            EPR = pd.read_csv(EPR_results,usecols=[0,port_index + 1])
+            EPR.columns = ["mode","EPR"]
+            return
+            
+        except:
+            raise ValueError("Are you sure you defined a port?")
+
+    def get_Sij(self,index1:int,index2:int):
+    
+        if self.config.sim["Problem"]["Type"] != "Driven":
+            raise ValueError("Simulation type is not Driven, no port S-parameters to extract")
+            
+        else:
+        
+            Smatrix_results = self.config.sim["Problem"]["Output"]+"/port-S.csv"
+            Smatrix = pd.read_csv(Smatrix_results)
+            
+            ReSij =  '             |S[{}][{}]| (dB)'.format(index1,index2)
+            ImSij =  '        arg(S[{}][{}]) (deg.)'.format(index1,index2)
+            
+            try:
+                f_GHz = Smatrix['        f (GHz)'].to_numpy()
+                ReSij_column = Smatrix[ReSij].to_numpy()
+                ImSij_column = Smatrix[ImSij].to_numpy()
+                columns = ["frequency_GHz","|S[{}][{}]| (dB)".format(index1,index2),"arg(S[{}][{}]) (deg)".format(index1,index2)]
+                Scustom = pd.Dataframe{columns[0]:f_GHz,columns[1]:ReSij_column,columns[2]:ImSij_column}
+                return Scustom
+                
+            except:
+                raise ValueError("Selected S_ij matrix elements do not exist, please check specified indices")
+
+        
